14d0bba9b539f53aeca8419733c3784f4aefc6e0
Yien Xu <yienxu@hotmail.com>
2018-12-14 23:33:44 -0600

    Fix style and remove unnecessary code

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -26,20 +26,21 @@ y = df['count'].astype(int)
 y = y - 2
 y = y.astype('category')
 
-# bkpt = int(X.shape[0] * 0.9)
-# x_train = X.values[:bkpt]
-# x_test = X.values[bkpt:]
-# y_train = y.values[:bkpt]
-# y_test = y.values[bkpt:]
-
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size=0.1,
                                                     random_state=123,
                                                     shuffle=True,
                                                     stratify=y)
-x_train = X_train.values
-x_test = X_test.values
 
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+                                                  test_size=0.1,
+                                                  random_state=123,
+                                                  shuffle=True,
+                                                  stratify=y_train)
+
+x_train = X_train.values
+x_val = X_val.values
+x_test = X_test.values
 
 batch_size = 128
 num_classes = 29
@@ -48,36 +49,43 @@ epochs = 50
 # input image dimensions
 img_rows, img_cols = 28, 28
 
-
-
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_val = x_val.reshape(x_val.shape[0], 1, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_val = x_val.reshape(x_val.shape[0], img_rows, img_cols, 1)
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
+print(x_val.shape[0], 'val samples')
 print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
 y_train = to_categorical(y_train, num_classes)
+y_val = to_categorical(y_val, num_classes)
 y_test = to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(64, kernel_size=(3, 3),
+model.add(Conv2D(filters=64,
+                 kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
-model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(filters=128,
+                 kernel_size=(3, 3),
+                 activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.3))
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
+model.add(Dense(256,
+                activation='relu'))
 model.add(Dropout(0.6))
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(num_classes,
+                activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
@@ -89,11 +97,10 @@ history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
-                    validation_data=(x_test, y_test))
-
-
-
+                    validation_data=(x_val, y_val))
+# model.save_weights('cnn.h5')
 print(history.history.keys())
+
 #  "Accuracy"
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
@@ -102,14 +109,6 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
 plt.show()
-# "Loss"
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'validation'], loc='upper left')
-# plt.show()
 
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
